ac2b99e8c0535d2e4b79189794d0d3894fa38d41
Maayan Hadasi <mguetta@redhat.com>
2023-04-02 12:37:19 +0300

    Modifying failed test cases
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- mta/tests/developer/controls/test_tags.py
+++ mta/tests/developer/controls/test_tags.py
@@ -11,5 +11,5 @@ def test_create_tag(tagcategories_ids, create_api, get_api, delete_api):
         api_tag = ApiTag(category=tag_category, name="Api Tag")
         new_tag = create_api.tags_post(api_tag.to_dict())
         new_tag_from_db = get_api.tags_id_get(new_tag.id)
-        check.equal(new_tag_from_db, new_tag, "Checking the created tag")
+        check.equal(new_tag_from_db.name, new_tag.name, "Checking the created tag's name")
         delete_api.tags_id_delete(str(new_tag.id))
