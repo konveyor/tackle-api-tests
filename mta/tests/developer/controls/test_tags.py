49148bc71220407dc99287a5d4b7ccd58e94a5db
Maayan Hadasi <mguetta@redhat.com>
2023-04-02 11:51:06 +0300

    Removing TCs from test_tags.py
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- mta/tests/developer/controls/test_tags.py
+++ mta/tests/developer/controls/test_tags.py
@@ -3,16 +3,6 @@ from pytest_check import check
 
 
 @pytest.mark.tags
-def test_default_tags(json_defaults, tag_names):
-    assert set(json_defaults["tags"]).issubset(tag_names), "Default tags check FAILED! (found : expected)"  # noqa: E501
-
-
-@pytest.mark.tags
-def test_default_tag_types(json_defaults, tag_types_names):
-    assert set(json_defaults["tag_types"]).issubset(tag_types_names), "Default tag types check FAILED!"
-
-
-@pytest.mark.tags
 def test_create_tag(tag_types_ids, create_api, get_api, delete_api):
     for tagtype_id in tag_types_ids:
         tag_data = {"name": "Api Tag", "tagType": {"id": tagtype_id}}
