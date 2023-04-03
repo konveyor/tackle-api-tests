a15e22c60c46088edb5fee1b3b13500d3e4d07df
Shveta Sachdeva <sshveta@redhat.com>
2023-04-03 07:52:04 -0700

    Merge pull request #38 from mguetta1/modify_tests
    [RFR] Modified failed test cases
    
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- mta/tests/developer/application_inventory/test_application.py
+++ mta/tests/developer/application_inventory/test_application.py
@@ -5,4 +5,4 @@ import pytest
 def test_create_app(applications, get_api):
     for app in applications:
         app_from_db = get_api.applications_id_get(app.id)
-        assert app == app_from_db
+        assert app.name == app_from_db.name
