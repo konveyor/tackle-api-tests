5554f93cb686807bf3d2de14454d25fa7728c415
Maayan Hadasi <mguetta@redhat.com>
2023-05-23 10:28:27 +0300

    Regenerate swagger_client
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- swagger_client/api/__init__.py
+++ swagger_client/api/__init__.py
@@ -3,13 +3,33 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from swagger_client.api.copy_api import CopyApi
-from swagger_client.api.create_api import CreateApi
-from swagger_client.api.delete_api import DeleteApi
-from swagger_client.api.export_api import ExportApi
-from swagger_client.api.get_api import GetApi
-from swagger_client.api.list_api import ListApi
-from swagger_client.api.post_api import PostApi
-from swagger_client.api.setting_api import SettingApi
-from swagger_client.api.update_api import UpdateApi
-from swagger_client.api.update_create_api import UpdateCreateApi
+from swagger_client.api.addons_api import AddonsApi
+from swagger_client.api.adoptionplans_api import AdoptionplansApi
+from swagger_client.api.analyses_api import AnalysesApi
+from swagger_client.api.applications_api import ApplicationsApi
+from swagger_client.api.auth_api import AuthApi
+from swagger_client.api.batch_api import BatchApi
+from swagger_client.api.buckets_api import BucketsApi
+from swagger_client.api.businessservices_api import BusinessservicesApi
+from swagger_client.api.cache_api import CacheApi
+from swagger_client.api.dependencies_api import DependenciesApi
+from swagger_client.api.file_api import FileApi
+from swagger_client.api.identities_api import IdentitiesApi
+from swagger_client.api.imports_api import ImportsApi
+from swagger_client.api.issuecomposites_api import IssuecompositesApi
+from swagger_client.api.issues_api import IssuesApi
+from swagger_client.api.jobfunctions_api import JobfunctionsApi
+from swagger_client.api.migrationwaves_api import MigrationwavesApi
+from swagger_client.api.proxies_api import ProxiesApi
+from swagger_client.api.reviews_api import ReviewsApi
+from swagger_client.api.rulesets_api import RulesetsApi
+from swagger_client.api.schema_api import SchemaApi
+from swagger_client.api.settings_api import SettingsApi
+from swagger_client.api.stakeholdergroups_api import StakeholdergroupsApi
+from swagger_client.api.stakeholders_api import StakeholdersApi
+from swagger_client.api.tagcategories_api import TagcategoriesApi
+from swagger_client.api.tags_api import TagsApi
+from swagger_client.api.taskgroups_api import TaskgroupsApi
+from swagger_client.api.tasks_api import TasksApi
+from swagger_client.api.tickets_api import TicketsApi
+from swagger_client.api.trackers_api import TrackersApi
