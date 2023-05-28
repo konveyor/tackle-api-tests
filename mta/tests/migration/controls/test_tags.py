7707eedc4d97f5ab89aae85d958b938ea67862d5
Maayan Hadasi <mguetta@redhat.com>
2023-05-28 13:28:33 +0300

    Resolving conflicts
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- mta/tests/migration/controls/test_tags.py
+++ mta/tests/migration/controls/test_tags.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_check import check
 
+from conftest import generate_string
 from swagger_client.models.api_tag import ApiTag
 
 
@@ -8,7 +9,7 @@ from swagger_client.models.api_tag import ApiTag
 def test_create_tag(tagcategories_ids, tags_api):
     for tag_category_id in tagcategories_ids:
         tag_category = {"id": tag_category_id}
-        api_tag = ApiTag(category=tag_category, name="Api Tag")
+        api_tag = ApiTag(category=tag_category, name=generate_string(start="Api Tag"))
         new_tag = tags_api.tags_post(api_tag.to_dict())
         new_tag_from_db = tags_api.tags_id_get(new_tag.id)
         check.equal(new_tag_from_db.name, new_tag.name, "Checking the created tag's name")
